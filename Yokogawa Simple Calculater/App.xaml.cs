4128b5f6588dcffa66a77daf194cc376373c5c08
jimes1107 <100204234+jimes1107@users.noreply.github.com>
2025-02-08 11:36:50 +0800

    Add project files.

---
+++ Yokogawa Simple Calculater/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Yokogawa_Simple_Calculater
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
